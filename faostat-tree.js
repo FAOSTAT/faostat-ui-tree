17b6d1078dff93ceefb6a91c4c130d0df05fbd2e
Kalimaha <guido.barbaglia@gmail.com>
2015-02-24 13:29:31 +0100

    Unused multilanguage files removed.

--- faostat-tree.js
+++ faostat-tree.js
@@ -1,10 +1,8 @@
 define(['jquery',
         'handlebars',
-        'text!faostat_tree/html/templates.html',
-        'i18n!faostat_tree/nls/translate',
         'bootstrap',
         'jstree',
-        'sweet-alert'], function ($, Handlebars, templates, translate) {
+        'sweet-alert'], function ($, Handlebars) {
 
     'use strict';
 
