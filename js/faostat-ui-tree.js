8a8dcfb3bd775dad7c6648be34f42ee7c7abd38e
Kalimaha <guido.barbaglia@gmail.com>
2015-04-22 16:25:07 +0200

    WDS Client.

--- js/faostat-ui-tree.js
+++ js/faostat-ui-tree.js
@@ -41,100 +41,86 @@ define(['jquery',
         /* Store JQuery object.. */
         this.tree = $('#' + _this.CONFIG.placeholder_id);
 
-        /* REST URL */
-        var url = this.CONFIG.url_rest + '/' + this.CONFIG.datasource + '/' + this.CONFIG.lang_faostat;
+        /* Fetch FAOSTAT groups and domains. */
+        Commons.wdsclient('groupsanddomains', this.CONFIG, function(json) {
 
-        /* Load groups and domains from the DB. */
-        $.ajax({
+            /* Buffer. */
+            var buffer = [];
+            var payload = [];
 
-            type: 'GET',
-            url: url,
+            /* Iterate over domains. */
+            for (var i = 0 ; i < json.length ; i++) {
 
-            success: function (response) {
-
-                /* Cast the response to JSON, if needed. */
-                var json = response;
-                if (typeof json == 'string')
-                    json = $.parseJSON(response);
-
-                /* Buffer. */
-                var buffer = [];
-                var payload = [];
-
-                /* Iterate over domains. */
-                for (var i = 0 ; i < json.length ; i++) {
-
-                    /* Create group node. */
-                    if ($.inArray(json[i][0], buffer) < 0) {
-                        buffer.push(json[i][0]);
-                        payload.push({
-                            id: json[i][0],
-                            text: json[i][1],
-                            parent: '#'
-                        });
-                    }
-
-                    /* Add domain node. */
+                /* Create group node. */
+                if ($.inArray(json[i][0], buffer) < 0) {
+                    buffer.push(json[i][0]);
                     payload.push({
-                        id: json[i][2],
-                        text: json[i][3],
-                        parent: json[i][0]
+                        id: json[i][0],
+                        text: json[i][1],
+                        parent: '#'
                     });
-
                 }
 
-                /* Init JSTree. */
-                _this.tree.jstree({
-
-                    'plugins': ['unique', 'search', 'types', 'wholerow'],
-
-                    'core': {
-                        'data': payload,
-                        'themes': {
-                            'icons': false,
-                            'responsive': true
-                        }
-                    },
-
-                    'search': {
-                        'show_only_matches': true,
-                        'close_opened_onclear': false
-                    }
-
-                });
-
-                _this.tree.on('select_node.jstree', function (e, data) {
-                    var node = $('#' + data.node.id);
-                    if (data.node.parent == '#')
-                        _this.tree.jstree().is_open() ? _this.tree.jstree().close_node(node) : _this.tree.jstree().open_node(node);
-                });
-
-                /* Check whether is group or domain. */
-                _this.tree.on('changed.jstree', function (e, data) {
-                    if (data.node.parent == '#') {
-                        amplify.publish(_this.CONFIG.prefix + 'group_event', {id: data.node.id});
-                    } else {
-                        amplify.publish(_this.CONFIG.prefix + 'domain_event', {id: data.node.id});
-                    }
-                });
-
-                /* Show required domain. */
-                _this.tree.on('ready.jstree', function (e, data) {
-                    var node;
-                    if (_this.CONFIG.group != null) {
-                        node = $('#' + _this.CONFIG.group.toUpperCase());
-                        _this.tree.jstree().open_node(node);
-                    }
-                    if (_this.CONFIG.domain != null) {
-                        node = $('#' + _this.CONFIG.domain.toUpperCase());
-                        _this.tree.jstree().select_node(node);
-                    } else {
-                        node = $('#' + _this.CONFIG.group.toUpperCase());
-                        _this.tree.jstree().select_node(node);
-                    }
+                /* Add domain node. */
+                payload.push({
+                    id: json[i][2],
+                    text: json[i][3],
+                    parent: json[i][0]
                 });
 
             }
+
+            /* Init JSTree. */
+            _this.tree.jstree({
+
+                'plugins': ['unique', 'search', 'types', 'wholerow'],
+
+                'core': {
+                    'data': payload,
+                    'themes': {
+                        'icons': false,
+                        'responsive': true
+                    }
+                },
+
+                'search': {
+                    'show_only_matches': true,
+                    'close_opened_onclear': false
+                }
+
+            });
+
+            /* Implement node selection. */
+            _this.tree.on('select_node.jstree', function (e, data) {
+                var node = $('#' + data.node.id);
+                if (data.node.parent == '#')
+                    _this.tree.jstree().is_open() ? _this.tree.jstree().close_node(node) : _this.tree.jstree().open_node(node);
+            });
+
+            /* Check whether is group or domain. */
+            _this.tree.on('changed.jstree', function (e, data) {
+                if (data.node.parent == '#') {
+                    amplify.publish(_this.CONFIG.prefix + 'group_event', {id: data.node.id});
+                } else {
+                    amplify.publish(_this.CONFIG.prefix + 'domain_event', {id: data.node.id});
+                }
+            });
+
+            /* Show required domain. */
+            _this.tree.on('ready.jstree', function (e, data) {
+                var node;
+                if (_this.CONFIG.group != null) {
+                    node = $('#' + _this.CONFIG.group.toUpperCase());
+                    _this.tree.jstree().open_node(node);
+                }
+                if (_this.CONFIG.domain != null) {
+                    node = $('#' + _this.CONFIG.domain.toUpperCase());
+                    _this.tree.jstree().select_node(node);
+                } else {
+                    node = $('#' + _this.CONFIG.group.toUpperCase());
+                    _this.tree.jstree().select_node(node);
+                }
+            });
 
         });
 
