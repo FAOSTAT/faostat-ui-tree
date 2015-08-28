927d0cfa81d8be14116c6533e635548684f73880
Kalimaha <guido.barbaglia@gmail.com>
2015-08-28 14:17:26 +0200

    FENIX standards applied to FAOSTAT tree.

--- src/js/paths.js
+++ src/js/paths.js
@@ -1,8 +1,12 @@
-define(function() {
+/*global define*/
+define(function () {
 
-    var config = {
+    'use strict';
+
+    return {
         paths: {
             FAOSTAT_UI_TREE: 'start',
+            faostat_ui_tree: '../',
             sweetAlert: '{FENIX_CDN}/js/sweet-alert/0.5.0/sweet-alert.min',
             jstree: '{FENIX_CDN}/js/jstree/3.0.8/dist/jstree.min'
         },
@@ -12,7 +16,5 @@ define(function() {
             }
         }
     };
-
-    return config;
 
 });
