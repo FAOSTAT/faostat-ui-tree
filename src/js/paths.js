3b6d1ed4ce7259bbebf7d667c7c4e61f8caaaa6f
simonemurzilli <simone.murzilli@fao.org>
2016-04-27 11:01:03 +0200

    fixed jstree path

--- src/js/paths.js
+++ src/js/paths.js
@@ -7,7 +7,8 @@ define(function () {
         paths: {
             FAOSTAT_UI_TREE: 'start',
             faostat_ui_tree: '../',
-            jstree: '{FENIX_CDN}/js/jstree/3.0.8/dist/jstree.min',
+            // jstree: '{FENIX_CDN}/js/jstree/3.0.8/dist/jstree.min',
+            jstree: '{FENIX_CDN}/js/jstree/3.3.0/dist/jstree.min',
             faostatapiclient: '{FENIX_CDN}/js/faosttapiclient/1.0.0/dist/FAOSTATAPIClient.min'
         },
         shim: {
